06d9eef4e6b591bf4313800aef8fbffef7207250
Nukepayload2 <nukepayload2@outlook.com>
2025-02-03 16:40:53 +0800

    Test completion

--- DeepseekAPIExamples/CodeExamples.vb
+++ DeepseekAPIExamples/CodeExamples.vb
@@ -1,9 +1,32 @@
 ﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports Nukepayload2.AI.Providers.Deepseek
+Imports Nukepayload2.AI.Providers.Deepseek.Models
 
 <TestClass>
 Public Class CodeExamples
-    <TestMethod>
-    Sub TestSub()
+    Private Shared ReadOnly Property ApiKey As String
+        Get
+            Dim key = Environment.GetEnvironmentVariable("DEEPSEEK_API_KEY")
+            If key = Nothing Then
+                Throw New InvalidOperationException("Please set API key to %DEEPSEEK_API_KEY% and try again.")
+            End If
+            Return key
+        End Get
+    End Property
 
-    End Sub
+    <TestMethod>
+    Async Function TestCompletionAsync() As Task
+        Dim clientV4 As New DeepSeekClient(ApiKey)
+        Dim request As New ChatRequest With {
+            .Model = ModelNames.ChatModel,
+            .Messages = {New ChatMessage With {.Role = ChatRoles.User, .Content = "你好，你是谁？"}},
+            .TopP = 0.9
+        }
+        Dim response = Await clientV4.Chat.CompleteAsync(request)
+
+        Dim respMessage = response.Choices?.FirstOrDefault?.Message?.Content
+        Console.WriteLine(respMessage)
+        Assert.IsNotNull(respMessage)
+    End Function
+
 End Class
